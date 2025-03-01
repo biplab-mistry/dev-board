84aa0acf793063410567353c04dd2b0d9bebde63
biplab-mistry <kr.biplabmistry@gmail.com>
2025-03-01 21:48:34 +0530

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -25,68 +25,59 @@ for (let btn of completeBtn) {
         }
         const containerElement = document.getElementById("right-container");
 
-        const card =btn.closest('.card');
-        const cardTitle=card.querySelector('.card-title').innerText;
-        
-        
-        const div=document.createElement("div");
+        const card = btn.closest('.card');
+        const cardTitle = card.querySelector('.card-title').innerText;
 
-        const currentTime = new Date().toLocaleTimeString('en-IN', { 
-            hour: '2-digit', 
-            minute: '2-digit', 
-            second: '2-digit', 
-            hour12: true 
+
+        const div = document.createElement("div");
+
+        const currentTime = new Date().toLocaleTimeString('en-IN', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: true
         });
 
         div.classList.add("beautify");
-        div.innerHTML=`
+        div.innerHTML = `
         <p>you have completed the task ${cardTitle} at ${currentTime}</p>
         
         `
         containerElement.style.display = 'block';
-
-
-        
         containerElement.appendChild(div)
-      
-        
-     
     })
-
-
-
 }
 
 // clean up
 
-document.getElementById("cleanUp").addEventListener("click",function(){
-    const rightContainer=document.getElementById("right-container")
-    rightContainer.style.display='none';
+document.getElementById("cleanUp").addEventListener("click", function () {
+    const rightContainer = document.getElementById("right-container")
+    rightContainer.style.display = 'none';
     rightContainer.innerHTML = '';
-    for(let btn of completeBtn){
-        if(btn.hasAttribute('disabled')){
+    for (let btn of completeBtn) {
+        if (btn.hasAttribute('disabled')) {
             btn.removeAttribute('disabled')
         }
     }
-    document.getElementById("task-assignment").innerText=completeBtn.length;
-    document.getElementById("nav-assignment").innerText=0;
+    document.getElementById("task-assignment").innerText = completeBtn.length;
+    document.getElementById("nav-assignment").innerText = 0;
 })
 // random color
-document.getElementById("colorChange").addEventListener("click",function(){
-    document.body.style.backgroundColor=backGroundColor();
+document.getElementById("colorChange").addEventListener("click", function () {
+    document.body.style.backgroundColor = backGroundColor();
 })
-function backGroundColor(){
-    return randomColor="#"+Math.floor(Math.random()*(256*256*256)).toString(16)
-    
+function backGroundColor() {
+    return randomColor = "#" + Math.floor(Math.random() * (256 * 256 * 256)).toString(16)
+
 }
 
-  // time show in deadline section
+// time show in deadline section
 //   const deadLine=document.querySelectorAll(".dead-line")
 
-    const deadLine=  document.querySelectorAll('.dead-line');
-   
-    for(let date of deadLine){
-        date.innerText=new Date().toDateString('en-IN')
-    }
-      
-  
+const deadLine = document.querySelectorAll('.dead-line');
+
+for (let date of deadLine) {
+    date.innerText = new Date().toDateString('en-IN')
+}
+
+
