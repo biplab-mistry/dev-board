131fcb654e22e4f0a1961cb69bbd2085628eb1a4
biplab-mistry <kr.biplabmistry@gmail.com>
2025-03-02 07:01:08 +0530

    css added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -77,7 +77,7 @@ function backGroundColor() {
 const deadLine = document.querySelectorAll('.dead-line');
 
 for (let date of deadLine) {
-    date.innerText = new Date().toDateString('en-IN')
+    date.innerText = new Date().toLocaleDateString('en-IN',options2)
 }
 
 
